be3d90c1fadc35332cc5226f11544a88ff6ec62c
Darrow Palast <dpalast@gmail.com>
2019-08-02 15:48:12 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,3 +3,5 @@ def introduction(name)
   puts "Hi, my name is #{name}."
   
 end
+
+#introduction_with_language that takes in two arguments, name and language and outputs the phrase: "Hi, my name is #{name} and I am learning to program in #{language}."
